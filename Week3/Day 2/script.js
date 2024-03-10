3ead47999b0537b0d435a5deb93a026a675e1393
aziim7 <aziim.mohammad7@gmail.com>
2024-03-10 23:04:58 +0400

    exercise 1

---
+++ Week3/Day 2/script.js
@@ -0,0 +1,6 @@
+var addressNumber = "5";
+var addressStreet = "BenYehuda";
+var country = "Palestine";
+
+var globalAddress = ("I live in "+addressStreet+" "+addressNumber+","+"in "+country);
+console.log(globalAddress);
