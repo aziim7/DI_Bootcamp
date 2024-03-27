86730b3331b8e306fc3333b880164658e0c8d327
aziim7 <aziim.mohammad7@gmail.com>
2024-03-27 23:44:18 +0400

    '27.03.2024'

---
+++ Week3/Day 2/ex3.js
@@ -0,0 +1,6 @@
+var pets = ['cat', 'dog', 'fish', 'rabbit', 'cow'];
+console.log(pets[1]);
+pets.push('horse');
+console.log(pets.length);
+console.log(pets);
+
--- Week3/Day 2/script.js
+++ Week3/Day 2/script.js
@@ -1,6 +1,16 @@
+
+// Exercise 1
 var addressNumber = "5";
 var addressStreet = "BenYehuda";
 var country = "Palestine";
 
 var globalAddress = ("I live in "+addressStreet+" "+addressNumber+","+"in "+country);
 console.log(globalAddress);
+
+// Exercise 2
+
+var birthyear = 1990; 
+var fyear = 2050; 
+var calculate = fyear - birthyear; 
+
+console.log("I will be "+calculate+ " in "+fyear);
